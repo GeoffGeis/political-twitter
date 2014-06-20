2b7f4de4ee287b7042863da4c7bec67029279ccf
GeoffGeis <geoffgeis@hotmail.com>
2014-06-20 02:11:25 -0500

    Update storm.rb
    replaced global var for instance

--- storm.rb
+++ storm.rb
@@ -3,7 +3,7 @@ require 'ssl_certifier'
 
 class Storm
   def initialize(cfg)
-    $client = Twitter::REST::Client.new do |config|
+    @client = Twitter::REST::Client.new do |config|
       config.consumer_key = cfg[:consumer_key]
       config.consumer_secret = cfg[:consumer_secret]
       config.access_token = cfg[:access_token]
@@ -12,7 +12,7 @@ class Storm
   end
 
   def tweet_this(message)
-    $client.update(message)
+    @client.update(message)
   end
 
   def tweet_all(tweets)
