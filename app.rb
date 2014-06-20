e9c1692983f02ac6b114f23096532c39d208cfae
GeoffGeis <geoffgeis@hotmail.com>
2014-06-20 04:48:21 -0500

    Update app.rb
    Updated with target feature mwhahahaha

--- app.rb
+++ app.rb
@@ -1,21 +1,30 @@
 require './storm'
 require 'ssl_certifier'
 
-def parse_file(f)
-  tweets = []
-  file = File.new(f, 'r')
-  file.each_line("\n") do |row|
-    tweets << row.chomp if row.chomp.length < 140
+class App
+  def initialize(target)
+    @tweets = parse_file(target, './tweets')
+    config = {}
+    config[:consumer_key] = "23vyq5tfgN9Mr2ZgmWWaQ"
+    config[:consumer_secret] = "8dUwJsl6bDS3Dn38gwKMatffXwZ4vh2SvDGPp1M"
+    config[:access_token] = "1475732792-5d4Q4LxcxgZZ2U27Db7maBA0lRmS2hg5HYXmhfm"
+    config[:access_token_secret] = "WE3hIyEhIIjPPb6yglaT512XPtiS9b01mHsSjsNdi0"
+    @client = Storm.new(config)
+    attack!
   end
-  tweets
+
+  def parse_file(target, f)
+    tweets = []
+    file = File.new(f, 'r')
+    file.each_line("\n") do |row|
+      tweets << target + ' ' + row.chomp if row.chomp.length < 140
+    end
+    tweets
+  end
+
+  def attack!
+    @client.tweet_all(@tweets)
+  end
 end
 
-tweets = parse_file './tweets'
-config = {}
-config[:consumer_key] = ""
-config[:consumer_secret] = ""
-config[:access_token] = ""
-config[:access_token_secret] = ""
-
-client = Storm.new(config)
-client.tweet_all tweets
+a = App.new('@target')
