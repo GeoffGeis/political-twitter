6084678db2f7f7a1c7cdc3f88441959964d87a95
GeoffGeis <geoffgeis@hotmail.com>
2014-06-20 04:48:58 -0500

    Update app.rb
    target feature added

--- app.rb
+++ app.rb
@@ -5,10 +5,10 @@ class App
   def initialize(target)
     @tweets = parse_file(target, './tweets')
     config = {}
-    config[:consumer_key] = "23vyq5tfgN9Mr2ZgmWWaQ"
-    config[:consumer_secret] = "8dUwJsl6bDS3Dn38gwKMatffXwZ4vh2SvDGPp1M"
-    config[:access_token] = "1475732792-5d4Q4LxcxgZZ2U27Db7maBA0lRmS2hg5HYXmhfm"
-    config[:access_token_secret] = "WE3hIyEhIIjPPb6yglaT512XPtiS9b01mHsSjsNdi0"
+    config[:consumer_key] = ""
+    config[:consumer_secret] = ""
+    config[:access_token] = ""
+    config[:access_token_secret] = ""
     @client = Storm.new(config)
     attack!
   end
